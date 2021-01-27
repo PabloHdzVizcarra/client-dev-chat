7d44dba15fd6a6aa4bf9b8974d74e2301cad47fd
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-01-26 22:32:03 -0600

    Feat: added hook useForm to handle the functionality of the form

---
+++ src/hooks/useForm/useForm.js
@@ -0,0 +1,57 @@
+import { useState, useEffect, useRef } from 'react'
+
+const useForm = ({ initialValues, onSubmit }) => {
+
+  const [values, setValues] = useState(initialValues || {})
+  const [errors, setErrors] = useState({})
+  const [touched, setTouched] = useState({})
+
+  const formRendered = useRef(true)
+
+  useEffect(() => {
+    if (formRendered.current) {
+      setValues(initialValues)
+      setErrors({})
+      setTouched({})
+    }
+
+    formRendered.current = false
+  }, [initialValues])
+
+  const handleChange = (event) => {
+    const { name, value } = event.target
+    event.persist()
+    setValues({ ...values, [name]: value })
+  }
+
+  const handleBlur = (event) => {
+    const {name} = event.target
+    setTouched({...touched, [name]: true})
+    setErrors({...errors})
+  }
+
+  const handleSubmit = event => {
+    if (event) event.preventDefault()
+    const {username} = values
+
+    if (username.length < 3) {
+      setErrors({username: 'must be greater than 3 characters'})
+      onSubmit({errors})
+      return
+    }
+
+    setErrors({})
+    onSubmit({values, errors})
+  }
+
+  return {
+    values,
+    errors,
+    touched,
+    handleChange,
+    handleBlur,
+    handleSubmit
+  }
+}
+
+export default useForm
