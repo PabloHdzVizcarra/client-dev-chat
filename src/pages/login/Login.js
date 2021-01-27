d1a8b61d32c82bd0d4b85536f104b8c392ae03df
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-01-27 13:16:48 -0600

    Feat: added login page

---
+++ src/pages/login/Login.js
@@ -0,0 +1,9 @@
+import RegisterTemplate from '../../components/templates/RegisterTemplate/RegisterTemplate'
+
+const Login = () => {
+  return (
+    <RegisterTemplate />
+  )
+}
+
+export default Login
