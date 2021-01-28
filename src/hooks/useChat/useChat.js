7ace9cb2c2d7f9c34a947a00f59a2d92e9535972
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-01-28 09:21:22 -0600

    Feat: new useChat hook to handle messages
    this hook will handle all the logic of the messages with the server

---
+++ src/hooks/useChat/useChat.js
@@ -0,0 +1,38 @@
+import { io } from 'socket.io-client'
+import { useState, useEffect } from 'react'
+import { useRef } from 'react'
+
+const NEW_CHAT_MESSAGE_EVENT = 'newMessage'
+const SOCKET_SERVER_URL = "http://0.0.0.0:3002"
+
+
+const useChat = (roomId) => {
+  const [messages, setMessages] = useState([])
+  const socket = useRef(io)
+
+  useEffect(() => {
+    socket.current = io(SOCKET_SERVER_URL, {
+      query: {roomId}
+    })
+
+    socket.current.on(NEW_CHAT_MESSAGE_EVENT, message => {
+      const incomingMessage = {
+        ...message,
+        ownedByCurrentUser: message.senderId === socket.current.id
+      }
+      setMessages(messages => [...messages, incomingMessage])
+    })
+
+  }, [roomId])
+
+  const sendMessage = messageBody => {
+    socket.current.emit(NEW_CHAT_MESSAGE_EVENT, {
+      body: messageBody,
+      senderId: socket.current.id
+    })
+  }
+
+  return {messages, sendMessage}
+}
+
+export default useChat
