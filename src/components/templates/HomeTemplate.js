098d973027b75e0e48d9471c0317e54a4b940240
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-01-28 13:09:18 -0600

    feat: HomeTemplate component is added

--- src/components/templates/HomeTemplate.js
+++ src/components/templates/HomeTemplate.js
@@ -1,6 +1,55 @@
-import HeaderH1 from '../atoms/HeaderH1/HeaderH1'
 import useChat from '../../hooks/useChat/useChat'
 import { useState } from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 15% 80% 5%;
+  height: 100vh;
+  background-color: #a0dbde;
+`
+
+const Paragraph = styled.p`
+  text-align: center;
+  font-size: 1.5rem;
+  margin: 20px 0 0 0;
+`
+
+const FlexContainer = styled.div`
+  display: flex;
+  background-color: #32bdc4;
+  padding: 5px;
+`
+
+const ContainerMessages = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: #0a8086;
+  overflow: auto;
+  padding: 10px;
+`
+
+const InputText = styled.input`
+  height: 100%;
+  width: 90%;
+`
+
+const Button = styled.button`
+  width: 10%;
+  height: 110%;
+  border-radius: 6px;
+  margin-left: 5px;
+  padding: 0;
+  border: none;
+`
+
+const Message = styled.div`
+  padding: 5px;
+  background-color: beige;
+  margin: 2px 0;
+  border-radius: 6px;
+  width: max-content;
+`
 
 const HomeTemplate = ({ roomID}) => {
 
@@ -16,30 +65,33 @@ const HomeTemplate = ({ roomID}) => {
     setNewMessage('')
   }
   return (
-    <div>
-      <HeaderH1 text={`Room: ${roomID}`}/>
-      <ol>
+    <Container>
+      <FlexContainer>
+        <Paragraph>{`Room: ${roomID}`}</Paragraph>
+      </FlexContainer>
+      <ContainerMessages>
         {messages.map(message => (
-          <li
-            key={Math.random()}
+          <Message
+            key={Date.now()}
           >
             {message.body}
-          </li>
+          </Message>
         ))}
-      </ol>
+      </ContainerMessages>
+      <FlexContainer>
+        <InputText
+          value={newMessage}
+          onChange={handleNewMessage}
+          placeholder='Write message...'
+        />
 
-      <textarea
-        value={newMessage}
-        onChange={handleNewMessage}
-        placeholder='Write message...'
-      />
-
-      <button
-        onClick={handleSendMessage}
-      >
-        Send
-      </button>
-    </div>
+        <Button
+          onClick={handleSendMessage}
+        >
+          Send
+        </Button>
+      </FlexContainer>
+    </Container>
     )
 }
 
