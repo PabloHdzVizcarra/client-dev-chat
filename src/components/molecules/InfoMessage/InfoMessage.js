428315dbc6f1ccef50857ba83b4aa66d08c6cc92
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-01-30 20:54:35 -0600

    feat: Added InfoMessage component

---
+++ src/components/molecules/InfoMessage/InfoMessage.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+
+const Container = styled.div`
+  position: fixed;
+  padding: 0.8rem;
+  margin: 8px;
+  right: 1px;
+  top: 1px;
+  border-radius: 8px;
+  background-color: rgba(255, 186, 0, 0.4);
+  width: auto;
+  text-align: center;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+`
+
+const Paragraph = styled.p`
+  margin: 0;
+  color: #423f3f;
+  font-size: 1rem;
+`
+
+function InfoMessage({ text }) {
+  return (
+    <Container>
+      <Paragraph>{text}</Paragraph>
+    </Container>
+  )
+}
+
+InfoMessage.propTypes = {
+  text: PropTypes.string.isRequired,
+}
+
+export default InfoMessage
