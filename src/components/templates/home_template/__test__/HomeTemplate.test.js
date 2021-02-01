5e07df60dbc38d43173ef13e778beb3ef120a1cb
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-02-01 08:11:48 -0600

    test: Delete old tests

--- src/components/templates/home_template/__test__/HomeTemplate.test.js
+++ src/components/templates/home_template/__test__/HomeTemplate.test.js
@@ -3,7 +3,6 @@ import {
   render,
   waitForElementToBeRemoved,
   screen,
-  fireEvent,
 } from '@testing-library/react'
 import HomeTemplate from '../HomeTemplate'
 import userEvent from '@testing-library/user-event'
@@ -46,76 +45,5 @@ describe('test in HomeTemplate', () => {
     userEvent.click(screen.getByRole('button'))
 
     expect(screen.getByRole('textbox')).toHaveTextContent('')
-  })
-
-  test('the message should be displayed on the screen when it it sent to the server', async () => {
-    const username = 'john'
-    const room = 'developer'
-    render(<HomeTemplate roomID={room} username={username} />)
-
-    // we verify that the info message is displayed on the screen
-    expect(
-      await screen.findByText(/john, welcome to room developer/),
-    ).toBeInTheDocument()
-
-    // add value to input and send message
-    userEvent.type(screen.getByRole('textbox'), 'hello world')
-    userEvent.click(screen.getByRole('button'))
-
-    expect(await screen.findByText(/hello world/)).toBeInTheDocument()
-  })
-  test('the same number of messages sent should be displayed on the screen', async () => {
-    const username = 'john'
-    const room = 'developer'
-    render(<HomeTemplate roomID={room} username={username} />)
-
-    // we verify that the info message is displayed on the screen
-    expect(
-      await screen.findByText(/john, welcome to room developer/),
-    ).toBeInTheDocument()
-
-    // add value to input and send message
-    userEvent.type(screen.getByRole('textbox'), 'hello world')
-    userEvent.click(screen.getByRole('button'))
-
-    // we verify that 1 li is displayed
-    await screen.findByText(/hello world/)
-    expect(screen.getAllByRole('listitem')).toHaveLength(1)
-
-    // add value to input and send message
-    userEvent.type(screen.getByRole('textbox'), 'best Javascript')
-    userEvent.click(screen.getByRole('button'))
-
-    // we verify that 2 li is displayed
-    await screen.findByText(/best Javascript/)
-    expect(screen.getAllByRole('listitem')).toHaveLength(2)
-  })
-
-  test('should display an error message when a message is successfully sent to the server, but the server does not correctly return the message', async () => {
-    const username = 'john'
-    const room = 'developer'
-    render(<HomeTemplate roomID={room} username={username} />)
-
-    // we verify that the message is displayed on the screen
-    const infoMessage = await screen.findByText(
-      /john, welcome to room developer/,
-    )
-    expect(infoMessage).toBeInTheDocument()
-
-    await waitForElementToBeRemoved(
-      screen.getByText(/john, welcome to room developer/),
-      { timeout: 5000 },
-    )
-
-    expect(
-      screen.queryByText(/john, welcome to room developer/),
-    ).not.toBeInTheDocument()
-
-    fireEvent.change(screen.getByRole('textbox'), {
-      target: { value: 'hello world' },
-    })
-    fireEvent.click(screen.getByRole('button'))
-
-    //await screen.findByTestId(/info-message/)
   })
 })
