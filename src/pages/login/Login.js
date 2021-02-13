59c0531be7af7ac04a879f29c826fc128b0cffb9
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-02-12 21:24:12 -0600

    Fix: RouterApp component

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,22 +1,7 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import RegisterTemplate from '../../components/templates/register-template/RegisterTemplate'
 
-const Login = ({ setUserData }) => {
-  //TODO: Form Validate
-  function submitForm(data) {
-    const { values } = data
-    console.log(values)
-
-    if (values?.username) {
-      setUserData(values)
-    }
-  }
-  return <RegisterTemplate submitForm={submitForm} />
-}
-
-Login.propTypes = {
-  setUserData: PropTypes.func.isRequired,
+const Login = () => {
+  return <p>Login Page</p>
 }
 
 export default Login
--- src/router/RouterApp.js
+++ src/router/RouterApp.js
@@ -3,18 +3,15 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  //Redirect,
+  Redirect,
 } from 'react-router-dom'
-// import Login from '../pages/login/Login'
-// import Home from '../pages/home/Home'
-// import checkUserData from './helpers/checkUserData'
+import Login from '../pages/login/Login'
+import Home from '../pages/home/Home'
 import Register from '../pages/register/Register'
 
 const RouterApp = () => {
-  // const [userData, setUserData] = React.useState({
-  //   username: '',
-  //   room: '',
-  // })
+  const [loginUser, setLoginUser] = React.useState(false)
+  console.log(setLoginUser)
   return (
     <Router>
       <div
@@ -24,16 +21,15 @@ const RouterApp = () => {
       >
         <Switch>
           <Route exact path='/'>
+            {!loginUser ? <Redirect to={'/login'} /> : <Home userData={''} />}
+            <Home userData={''} />
+          </Route>
+          <Route path='/register'>
             <Register />
           </Route>
-
-          {/*<Route path='/home'>*/}
-          {/*  {!checkUserData(userData) ? (*/}
-          {/*    <Redirect to={'/'} />*/}
-          {/*  ) : (*/}
-          {/*    <Home userData={userData} />*/}
-          {/*  )}*/}
-          {/*</Route>*/}
+          <Route path='/login'>
+            <Login />
+          </Route>
         </Switch>
       </div>
     </Router>
