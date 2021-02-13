3939a3d25b28ddb2c82a848cbaf6dae245beac21
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-02-12 21:07:55 -0600

    Feat: Redirect to page
    when a user successfully creates an account, he/she is redirect to the login page

--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 import RegisterTemplate from '../../components/templates/register-template/RegisterTemplate'
 
 function Register() {
+  let history = useHistory()
   async function handleSubmit(data) {
     const { username, password, email, colorUser } = data
 
-    const result = await fetch('http://localhost:3100/api/user', {
+    const response = await fetch('http://localhost:3100/api/user', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -17,8 +19,10 @@ function Register() {
         color: colorUser,
       }),
     })
-
-    console.log(await result.json())
+    if (response.status === 400) {
+      return
+    }
+    history.push('/login')
   }
   return <RegisterTemplate submitForm={handleSubmit} />
 }
