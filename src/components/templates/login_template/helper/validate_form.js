5aa5f18e55b80d9d8a3feefdc46cc88d57be7b60
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-02-13 21:00:38 -0600

    Refactor: validateForm function

--- src/components/templates/login_template/helper/validate_form.js
+++ src/components/templates/login_template/helper/validate_form.js
@@ -1,12 +1,12 @@
-function validateForm(data) {
-  const { email } = data
+function validateForm({ email }) {
   const regex = /\S+@\S+\.\S+/
   const errors = {}
 
   if (!regex.test(email)) {
     errors.email = 'El email que ingresaste no es valido'
-    return errors
   }
+
+  if (Object.keys(errors).length !== 0) return errors
 }
 
 export default validateForm
