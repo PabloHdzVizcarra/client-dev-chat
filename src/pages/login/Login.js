89d1d8ac25363246380c7b6bf0850ef4b286c375
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-02-14 07:29:40 -0600

    Feat: onSubmit function call API

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -2,8 +2,18 @@ import React from 'react'
 import LoginTemplate from '../../components/templates/login_template/LoginTemplate'
 
 const Login = () => {
-  function onSubmit(data) {
-    console.log(data)
+  async function onSubmit({ email, password }) {
+    const response = await fetch('http://localhost:3100/api/user/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    })
+    console.log(await response.json())
   }
   return <LoginTemplate onSubmit={onSubmit} />
 }
