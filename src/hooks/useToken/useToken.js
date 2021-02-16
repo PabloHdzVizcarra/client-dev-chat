7dbee402a7fbbd3b0558d0966f985cf0cfc336d5
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-02-16 11:18:50 -0600

    Feat: the token is stored in a cookie

--- src/hooks/useToken/useToken.js
+++ src/hooks/useToken/useToken.js
@@ -10,6 +10,7 @@ function useToken() {
 
   function saveToken(userToken) {
     sessionStorage.setItem('token', JSON.stringify(userToken))
+    document.cookie = `token=${userToken}`
     setToken(userToken.token)
   }
 
