2397b39ed04dfb8fa7a548b02c28c8614819daa6
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-02-23 19:54:34 -0600

    refactor styles move to another file

--- src/components/molecules/Select/Select.js
+++ src/components/molecules/Select/Select.js
@@ -1,28 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { v4 as uuidv4 } from 'uuid'
-import styled from 'styled-components'
-
-const Container = styled.div`
-  padding: 5px;
-  display: flex;
-  justify-content: center;
-  margin: 20px 0 0 0;
-`
-
-const ESelect = styled.select`
-  padding: 10px;
-  width: 20%;
-  text-align: center;
-  text-align-last: center;
-  font-size: 1.1rem;
-  border: 2px solid #212121;
-  background-color: white;
-`
-
-const Option = styled.option`
-  text-align: center;
-`
+import { Container, ESelect, Option } from './styles'
 
 Select.propTypes = {
   data: PropTypes.array.isRequired,
@@ -31,7 +10,6 @@ Select.propTypes = {
 }
 
 function Select({ data, handleChange, valueSelect }) {
-  React.useEffect(() => {}, [valueSelect])
   return (
     <Container>
       <ESelect onChange={(e) => handleChange(e)} value={valueSelect}>
---
+++ src/components/molecules/Select/styles.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  padding: 5px;
+  display: flex;
+  justify-content: center;
+  margin: 20px 0 0 0;
+`
+
+export const ESelect = styled.select`
+  padding: 10px;
+  width: 20%;
+  text-align: center;
+  text-align-last: center;
+  font-size: 1.1rem;
+  border: 2px solid #212121;
+  background-color: white;
+`
+
+export const Option = styled.option`
+  text-align: center;
+`
