5400d8feef5f1d12a13f95175ed1b268cf8e0f4b
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-03-02 07:17:51 -0600

    feat click button display lateral menu

--- src/components/organisms/UsersAreaMobile/UsersAreaMobile.js
+++ src/components/organisms/UsersAreaMobile/UsersAreaMobile.js
@@ -1,18 +1,40 @@
 import React from 'react'
 import styled from 'styled-components'
 // import PropTypes from 'prop-types'
-import { IconMessages } from '../../templates/home_template/styles'
+import {
+  CloseIcon,
+  ContainerCloseIcon,
+  ContainerDataUsers,
+  IconMessages,
+} from '../../templates/home_template/styles'
 
 const ContainerUsersAreaMobile = styled.div`
   cursor: pointer;
 `
 
 UsersAreaMobile.propTypes = {}
+function DataUsers() {
+  return (
+    <ContainerDataUsers>
+      <ContainerCloseIcon>
+        <CloseIcon />
+      </ContainerCloseIcon>
+      data user
+    </ContainerDataUsers>
+  )
+}
 
 function UsersAreaMobile() {
+  const [isDisplay, setIsDisplay] = React.useState(false)
+
+  function handleClick() {
+    console.log('click')
+    setIsDisplay((state) => !state)
+  }
   return (
     <ContainerUsersAreaMobile>
-      <IconMessages />
+      <IconMessages onClick={handleClick} />
+      {isDisplay ? <DataUsers /> : null}
     </ContainerUsersAreaMobile>
   )
 }
--- src/components/templates/home_template/styles.js
+++ src/components/templates/home_template/styles.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 import { device } from '../../../utils/size'
 import { AiOutlineMessage } from 'react-icons/ai'
+import { ImCross } from 'react-icons/all'
 
 export const ContainerHomeTemplate = styled.div`
   display: grid;
@@ -43,4 +44,25 @@ export const IconMessages = styled(AiOutlineMessage)`
   color: rgb(65, 199, 199);
   font-size: 2rem;
   margin-left: 5px;
+`
+
+export const ContainerDataUsers = styled.div`
+  background-color: rgba(22, 97, 203, 0.9);
+  position: fixed;
+  top: 0;
+  width: 150px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`
+export const ContainerCloseIcon = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`
+
+export const CloseIcon = styled(ImCross)`
+  padding-right: 5px;
+  padding-top: 5px;
+  cursor: pointer;
+  color: #fe2c34;
 `
